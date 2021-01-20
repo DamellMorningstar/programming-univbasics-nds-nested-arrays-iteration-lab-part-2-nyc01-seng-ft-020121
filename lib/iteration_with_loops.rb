3b4ee38c91fb40b35b084c631e09ace3116c0a37
Da-mell Faines <damellfaines8@gmail.com>
2021-01-20 17:49:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,11 +9,11 @@ def find_min_in_nested_arrays(src)
     element_index = 0
     while element_index < src[row_index].length do
        inner_element = src[row_index][element_index]
-       lowest_temps << inner_element.flatten.min
+       lowest_temps << inner_element
       element_index +=1
     end
     row_index +=1
   end
-lowest_temps
+lowest_temps.min { |a, b|  }
 #lowest_temps << inner_element
 end
